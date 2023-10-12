ac9cf55f89c3c8b7308b00d2e5bdb318db8c0865
Amit Mourya <amit.mourya@jocata.com>
2023-10-12 12:09:31 +0530

    JSWD-0019: Angular Initial Setup

---
+++ src/app/app.module.ts
@@ -0,0 +1,30 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './home/home.component';
+import { IndexComponent } from './index/index.component';
+import { MethodologyComponent } from './methodology/methodology.component';
+import { RoadmapComponent } from './roadmap/roadmap.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent,
+    IndexComponent,
+    MethodologyComponent,
+    RoadmapComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
