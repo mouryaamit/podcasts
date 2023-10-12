0e64dc366ce7c8ac3d7b91f6b6bd44cc947233fe
Amit Mourya <amit.mourya@jocata.com>
2023-10-12 17:36:20 +0530

    JSWD-0008: Angular Update

--- src/app/index/index.component.ts
+++ src/app/index/index.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import * as d3 from 'd3';
-
+import { environment } from 'src/environments/environment';
 @Component({
   selector: 'app-index',
   templateUrl: './index.component.html',
@@ -11,6 +11,8 @@ export class IndexComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    const url = environment.graphUrl+"sumpoorn_test_json1.json?date="+new Date();
   }
 
 }
+
