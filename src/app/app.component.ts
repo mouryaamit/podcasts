dc4992ece27d9d21eb80c211771410c6f31c14a9
Azhar Parvaiz <azhar.parvaiz@jocata.com>
2023-10-18 17:20:55 +0530

    JSWD-0164 Mobile-Home Responsive Changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,6 +10,8 @@ export class AppComponent implements OnInit {
   title = 'app';
 
   ngOnInit() {
-    AOS.init();
+    AOS.init({
+      // disable: window.innerWidth < 768
+    });
   }
 }
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,15 +1,33 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit, HostListener } from '@angular/core';
+import { Router, NavigationEnd } from '@angular/router';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent implements OnInit {
+  appHeaderShadow = false;
+  isResponsive = false;
 
-  constructor() { }
+  @HostListener('window:scroll', [])
+  onWindowScroll() {
+    const scroll = window.scrollY;
+    this.appHeaderShadow = scroll > 0;
+  }
+
+  toggleMenu() {
+    this.isResponsive = !this.isResponsive;
+  }
+
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
+    // Subscribe to router events to automatically close the menu on navigation
+    this.router.events.subscribe((event) => {
+      if (event instanceof NavigationEnd && this.isResponsive) {
+        this.isResponsive = false;
+      }
+    });
   }
 
 }
