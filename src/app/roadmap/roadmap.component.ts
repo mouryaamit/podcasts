e1f08201383b53eb90cf1c7f8f1981cc4fcace67
raj.kumawat <raj.kumawat@jocata.com>
2023-11-07 21:02:11 +0530

    JSWD-153 roadmap subscribe api integration

--- src/app/roadmap/roadmap.component.ts
+++ src/app/roadmap/roadmap.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms'
+import { ApiService } from '../services/api.service';
+import { SumpoornApiService } from '../services/sumpoorn-api.service';
 
 @Component({
   selector: 'app-roadmap',
@@ -7,9 +10,56 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RoadmapComponent implements OnInit {
 
-  constructor() { }
+  subscribeGroup: FormGroup;
+  constructor(
+    private apiService: ApiService,
+    public sumpoornApiService : SumpoornApiService,
+    private fb: FormBuilder,
+  ) { 
+    this.createForm();
+  }
 
   ngOnInit(): void {
+    this.addValidations();
+  }
+
+  createForm(){
+    this.subscribeGroup = this.fb.group({
+      firstName: [""],
+      lastName: [""],
+      businessEmail: [""],
+      jobTitle: [""],
+      company: [""],
+      contactNumber: [""],
+      industry: [""],
+      checkedTermsAndConditions: [""]
+    })
+  }
+
+  addValidations(){
+    this.subscribeGroup.get('firstName').setValidators([Validators.required]);
+    this.subscribeGroup.get('lastName').setValidators([Validators.required]);
+    this.subscribeGroup.get('businessEmail').setValidators([Validators.required, Validators.pattern(/^[a-zA-Z0-9]+[a-zA-Z0-9.!#$%&'*+-/=?^_`{]+@[a-zA-Z0-9!#$%&'*+-/=?^_`{]+\.[a-zA-Z.]{2,8}$/)]);
+    this.subscribeGroup.get('jobTitle').setValidators([Validators.required]);
+    this.subscribeGroup.get('company').setValidators([Validators.required]);
+    this.subscribeGroup.get('contactNumber').setValidators([Validators.pattern(/^((91|0))?[0-9]{10,12}$/)]);
+    // this.subscribeGroup.get('industry').setValidators([Validators.required]);
+    this.subscribeGroup.get('checkedTermsAndConditions').setValidators([Validators.required]);
+  }
+
+  saveSubscription(){
+    let postData = this.subscribeGroup.getRawValue();
+
+    this.sumpoornApiService.saveSubscriptionDetails(postData).then(
+      (resp: any) => {
+          
+      },
+      (error) => {
+          console.error(error);
+          
+      }
+  );
+    
   }
 
 }
