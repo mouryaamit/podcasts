ab5c329b03eb58f0b9cccef130a62639d545ca79
Azhar Parvaiz <azhar.parvaiz@jocata.com>
2023-12-11 18:12:33 +0530

    JSWD-332 SEO Optimization

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,27 +2,54 @@ import { Component, OnInit } from '@angular/core';
 import * as AOS from 'aos';
 import * as $ from 'jquery';
 import { environment } from 'src/environments/environment';
+import { MetaService } from './services/meta.service';
+import { ActivatedRoute, Router, NavigationEnd } from '@angular/router';
+import { filter, map, mergeMap } from 'rxjs/operators';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent implements OnInit {
-  title = 'app';
+  constructor(
+    private metaService: MetaService,
+    private activatedRoute: ActivatedRoute,
+    private router: Router
+  ) {}
 
   ngOnInit() {
     AOS.init({
       // disable: window.innerWidth < 768
     });
+
     console.info('Last release timestamp is set to:', environment.timeStamp);
+
+    // Update SEO Tags
+    this.router.events
+      .pipe(
+        filter((event) => event instanceof NavigationEnd),
+        map(() => this.activatedRoute),
+        map((route) => {
+          while (route.firstChild) route = route.firstChild;
+          return route;
+        }),
+        filter((route) => route.outlet === 'primary'),
+        mergeMap((route) => route.data)
+      )
+      .subscribe((event) => {
+        this.metaService.updateTitle(event['title']);
+        this.metaService.updateDescription(event['description']);
+        this.metaService.updateKeywords(event['keywords']);
+        this.metaService.updateCanonicalLink();
+      });
   }
 
-  hideErrorModal(){
-    $("#errorsModal").hide();
-    $(".error_text").show();
-    $(".error_text_dynamic").hide();
+  hideErrorModal() {
+    $('#errorsModal').hide();
+    $('.error_text').show();
+    $('.error_text_dynamic').hide();
     $('.modal-backdrop').remove();
-    $(document.body).removeClass("modal-open");
-    $(document.body).removeAttr("style");
+    $(document.body).removeClass('modal-open');
+    $(document.body).removeAttr('style');
   }
 }
