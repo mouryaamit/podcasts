6876977abe831c5a567400cf32d89a6aa5706d9d
Kavya Renigunta <kavya.renigunta@jocata.com>
2024-03-11 13:50:34 +0530

    JSWD-356 Susbscribe popup and dynamic error message changes

--- src/app/subscribe-button/subscribe-button.component.ts
+++ src/app/subscribe-button/subscribe-button.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { SubscribeModelComponent } from '../dialog/subscribe-model/subscribe-model.component';
+import {
+  MatDialog,
+  MatDialogRef,
+  MAT_DIALOG_DATA,
+} from '@angular/material/dialog';
 
 @Component({
   selector: 'app-subscribe-button',
@@ -8,13 +14,28 @@ import { Component, OnInit } from '@angular/core';
 export class SubscribeButtonComponent implements OnInit {
   isAddIcon: boolean = true;
 
-  constructor() { }
+  constructor(public dialog: MatDialog) { }
 
   ngOnInit(): void {
   }
 
   toggleIcon() {
     this.isAddIcon = !this.isAddIcon;
+  }
+
+  openDialog(): void {
+    const dialogRef = this.dialog.open(SubscribeModelComponent, {
+      disableClose: true,
+      panelClass: 'app_generic_modal',
+      data: {
+        data: { mobileNumber: '' },
+      },
+    });
+    dialogRef.afterClosed().subscribe((result) => {
+      if (result) {
+      } else {
+      }
+    });
   }
 
 }
