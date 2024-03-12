29a2b49e98d28539109a6b667795b031b2fc61a8
Kavya Renigunta <kavya.renigunta@jocata.com>
2024-03-12 15:14:34 +0530

    JSWD-356 removed unused code

--- src/app/roadmap/roadmap.component.ts
+++ src/app/roadmap/roadmap.component.ts
@@ -1,13 +1,7 @@
-import { Component, Inject, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { ApiService } from '../services/api.service';
-import { SumpoornApiService } from '../services/sumpoorn-api.service';
-import * as $ from 'jquery';
-import { ToastrService } from 'ngx-toastr';
+import { Component, OnInit } from '@angular/core';
 import {
   MatDialog,
-  MatDialogRef,
-  MAT_DIALOG_DATA,
+  MatDialogRef
 } from '@angular/material/dialog';
 import { RoadmapModelComponent } from '../dialog/roadmap-model/roadmap-model.component';
 
@@ -17,72 +11,16 @@ import { RoadmapModelComponent } from '../dialog/roadmap-model/roadmap-model.com
   styleUrls: ['./roadmap.component.scss'],
 })
 export class RoadmapComponent implements OnInit {
-  subscribeGroup!: FormGroup;
   constructor(
-    private apiService: ApiService,
-    public sumpoornApiService: SumpoornApiService,
-    private fb: FormBuilder,
-    private toastr: ToastrService,
-    private dialog: MatDialog
-  ) {
-    this.createForm();
-  }
+    private dialog: MatDialog) {}
 
-  ngOnInit(): void {
-    this.addValidations();
-  }
+  ngOnInit(): void {}
 
-  createForm() {
-    this.subscribeGroup = this.fb.group({
-      firstName: [''],
-      lastName: [''],
-      businessEmail: [''],
-      jobTitle: [''],
-      company: [''],
-      contactNumber: [''],
-      industry: [''],
-      checkedTermsAndConditions: [false],
-    });
-  }
-
-  addValidations() {
-    this.subscribeGroup.get('firstName')?.setValidators([Validators.required]);
-    this.subscribeGroup.get('lastName')?.setValidators([Validators.required]);
-    this.subscribeGroup
-      .get('businessEmail')
-      ?.setValidators([
-        Validators.required,
-        Validators.pattern(
-          /^[a-zA-Z0-9]+[a-zA-Z0-9.!#$%&'*+-/=?^_`{]+@[a-zA-Z0-9!#$%&'*+-/=?^_`{]+\.[a-zA-Z.]{2,8}$/
-        ),
-      ]);
-    this.subscribeGroup.get('jobTitle')?.setValidators([Validators.required]);
-    this.subscribeGroup.get('company')?.setValidators([Validators.required]);
-    this.subscribeGroup
-      .get('contactNumber')
-      ?.setValidators([Validators.pattern(/^[9876]\d{9}$/)]);
-    // this.subscribeGroup.get('industry')?.setValidators([Validators.required]);
-    this.subscribeGroup
-      .get('checkedTermsAndConditions')
-      ?.setValidators([Validators.requiredTrue]);
-  }
-
-  openDialog(string) {
-    let panelClassToAdd = '';
-
-    if (string === 'subscribeModal') {
-      panelClassToAdd = 'app_generic_modal';
-    } else if (string === 'HSNModal') {
-      panelClassToAdd = 'app_hsn_modal';
-    }
+  openDialog() {
 
     const dialogRef = this.dialog.open(RoadmapModelComponent, {
       disableClose: true,
-      panelClass: panelClassToAdd,
-      data: {
-        data: { mobileNumber: '' },
-        type: string,
-      },
+      panelClass: 'app_hsn_modal'
     });
     dialogRef.afterClosed().subscribe((result) => {
       if (result) {
@@ -90,70 +28,4 @@ export class RoadmapComponent implements OnInit {
       }
     });
   }
-
-  saveSubscription() {
-    if (this.subscribeGroup.invalid) {
-      return;
-    }
-
-    let postData = this.subscribeGroup.getRawValue();
-
-    this.sumpoornApiService.saveSubscriptionDetails(postData).then(
-      (resp: any) => {
-        this.subscribeGroup.reset();
-        this.toastr.success(
-          'You will receive monthly updates of the Jocata Sumpoorn Index.',
-          "You've successfully subscribed",
-          {
-            timeOut: 10000,
-            extendedTimeOut: 5000,
-            positionClass: 'toast-bottom-center',
-            progressBar: true,
-            progressAnimation: 'increasing',
-            closeButton: true,
-          }
-        );
-        // this.hideSubscribeModal();
-      },
-      (error) => {
-        this.subscribeGroup.reset();
-        this.toastr.error(
-          'We are unable to process your request. Please try again after sometime.',
-          '',
-          {
-            timeOut: 10000,
-            extendedTimeOut: 5000,
-            positionClass: 'toast-bottom-center',
-            progressBar: true,
-            progressAnimation: 'increasing',
-            closeButton: true,
-          }
-        );
-
-        // if (error && error.statusMessage) {
-        //   $('.error_text_dynamic').html(error.statusMessage);
-        //   $('.error_text').hide();
-        //   $('.error_text_dynamic').show();
-        // } else {
-        //   $('.error_text').show();
-        //   $('.error_text_dynamic').hide();
-        // }
-        // this.hideSubscribeModal();
-      }
-    );
-  }
-
-  // hideSuccessModal() {
-  //   $('#successModal').hide();
-  //   $('.modal-backdrop').remove();
-  //   $(document.body).removeClass('modal-open');
-  //   $(document.body).removeAttr('style');
-  // }
-
-  // hideSubscribeModal(){
-  //   $("#subscribeModal").hide();
-  //   $('.modal-backdrop').remove();
-  //   $(document.body).removeClass("modal-open");
-  //   $(document.body).removeAttr("style");
-  // }
 }
