8a4184e0ffd2cddcbea23b925adae3839e26ce94
Azhar Parvaiz <azhar.parvaiz@jocata.com>
2024-04-25 16:39:28 +0530

    JSWD-411 Sumpoorn Website Update: Prashant's Live Presentation at UN Conference

--- src/app/dialog/united-nations/united-nations.component.ts
+++ src/app/dialog/united-nations/united-nations.component.ts
@@ -2,20 +2,14 @@ import { Component, Inject, OnInit } from '@angular/core';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 
 @Component({
-    selector: 'app-united-nations',
-    templateUrl: './united-nations.component.html',
-    styleUrls: ['./united-nations.component.scss']
-  })
+  selector: 'app-united-nations',
+  templateUrl: './united-nations.component.html',
+  styleUrls: ['./united-nations.component.scss'],
+})
+export class UnitedNationsModelComponent implements OnInit {
+  constructor(private dialogRef: MatDialogRef<UnitedNationsModelComponent>) {}
 
-export class UnitedNationsModelComponent implements OnInit{
-
-  constructor(private dialogRef: MatDialogRef<UnitedNationsModelComponent>
-  ) {
-  }
-
-  ngOnInit(): void {
-
-  }
+  ngOnInit(): void {}
 
   hideModal() {
     this.dialogRef.close('close');
